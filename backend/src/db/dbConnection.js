c901bbcefdd82bee8ab8b33d789570fb06e3ba05
jenish-vadodariya <202301202@dau.ac.in>
2025-09-19 21:03:49 +0530

    Just Brief Change in dbConnection file

--- backend/src/db/dbConnection.js
+++ backend/src/db/dbConnection.js
@@ -4,12 +4,12 @@ dotenv.config();
 import { neon } from "@neondatabase/serverless";
 
 let sql;
-function connectDB() {
-    try {
-        sql = neon(process.env.DATABASE_URL);
-        console.log("DB Connected");
-    } catch (error) {
-        console.error("Error connecting to the database:", error);
-    }
+
+try {
+    sql = neon(process.env.DATABASE_URL);
+    console.log("DB Connected");
+} catch (error) {
+    console.error("Error connecting to the database:", error);
 }
+
 export { sql };
