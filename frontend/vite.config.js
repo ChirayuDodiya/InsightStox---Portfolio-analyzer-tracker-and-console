1d7033c2b508daa5be8d96858e1b4b7b26f9c1b6
Jenil202301249 <202301249@dau.ac.in>
2025-10-13 22:27:32 +0530

    Add dotenv for environment variable management and update .gitignore

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import dotenv from 'dotenv'
+dotenv.config()
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://insightstox-portfolio-analyzer-tracker-j1n1.onrender.com',
+      '/api': process.env.BACKEND_LINK,
     }
   },
   plugins: [react(), tailwindcss()],
